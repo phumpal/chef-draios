d20dafa163d1407f1016b0ecd7a48e21d4e32fc7
phumpal <patrick@netvilla.net>
2014-02-21 14:29:27 -0800

    Bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer "Patrick Humpal"
 maintainer_email "patrick@netvilla.net"
 license "Apache 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.1"
+version "0.1.0"
 
 recipe "draios", "Deploys Draios"
 
